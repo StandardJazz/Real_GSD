50ea977fe668d66f5537a168f4dd689a2afd47d7
StandardJazz <201311275@sangmyung.kr>
2016-11-13 19:12:54 +0900

    마지막 수정
    프리팹 설정 계속 오류남.
    실제화 시키는것.
    라인을 연결하면 <이것을 인식하고
    병력이동하는것 다음에 할것.

--- Assets/Scripts/TroopCrtl.cs
+++ Assets/Scripts/TroopCrtl.cs
@@ -4,15 +4,24 @@ using System.Collections;
 public class TroopCrtl : MonoBehaviour {
 
 
+
 	float troopSpeed = 5.0f;
 	// Use this for initialization
 	void Start () {
+		
+
 	
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		Vector2 dir = (Camera.main.ScreenToWorldPoint(Input.mousePosition)- GameObject.Find("Troop").transform.position);
-		GetComponent<Transform> ().transform.Translate (dir.normalized * troopSpeed * Time.deltaTime);
-	}
+
+		void Update () {
+		if (GameObject.Find ("Line")==true) {
+				Debug.Log ("하하하하");
+				
+			//	Vector2 dir = (Camera.main.ScreenToWorldPoint (Input.mousePosition) - GameObject.Find ("Troop").transform.position);
+			//	GetComponent<Transform> ().transform.Translate (dir.normalized * troopSpeed * Time.deltaTime);
+			}
+		}
+		
 }
